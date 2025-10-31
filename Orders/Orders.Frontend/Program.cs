c5ba3b68a5d119ee195711872bbf31e3708717df
Servando Hernandez <s_musicpia@yahoo.com.mx>
2025-10-31 15:48:47 -0600

    Preparing Frontend to consume Backend

--- Orders/Orders.Frontend/Program.cs
+++ Orders/Orders.Frontend/Program.cs
@@ -1,5 +1,6 @@
 using MudBlazor.Services;
 using Orders.Frontend.Components;
+using Orders.Frontend.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ builder.Services.AddMudServices();
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri("https://localhost:7069") });
+builder.Services.AddScoped<IRepository, Repository>();
 
 var app = builder.Build();
 
