a44dbaa02cd337ce63fdd8b3c3d0b8b6d5f8d5ed
Servando Hernandez <s_musicpia@yahoo.com.mx>
2025-11-10 09:08:59 -0600

    Add Logout Sesion

--- Orders/Orders.Frontend/Components/Pages/Auth/Logout.razor.cs
+++ Orders/Orders.Frontend/Components/Pages/Auth/Logout.razor.cs
@@ -1,6 +1,25 @@
+using Microsoft.AspNetCore.Components;
+
+using MudBlazor;
+
+using Orders.Frontend.Services;
+
 namespace Orders.Frontend.Components.Pages.Auth;
 
 public partial class Logout
 {
+    [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+    [Inject] private ILoginService LoginService { get; set; } = null!;
+    [CascadingParameter] private IMudDialogInstance MudDialog { get; set; } = null!;
 
+    private async Task LogoutActionAsync()
+    {
+        await LoginService.LogoutAsync();
+        CancelAction();
+    }
+
+    private void CancelAction()
+    {
+        MudDialog.Cancel();
+    }
 }
