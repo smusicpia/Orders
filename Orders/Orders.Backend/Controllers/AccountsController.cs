0908e4d9af855a8279ab5efcb861c8ee37aa4eb9
Servando Hernandez <s_musicpia@yahoo.com.mx>
2025-11-10 14:26:17 -0600

    Add ChangePassword.

--- Orders/Orders.Backend/Controllers/AccountsController.cs
+++ Orders/Orders.Backend/Controllers/AccountsController.cs
@@ -109,6 +109,29 @@ public class AccountsController : ControllerBase
         return Ok(await _usersUnitOfWork.GetUserAsync(User.Identity!.Name!));
     }
 
+    [HttpPost("changePassword")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public async Task<IActionResult> ChangePasswordAsync(ChangePasswordDTO model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+
+        var user = await _usersUnitOfWork.GetUserAsync(User.Identity!.Name!);
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        var result = await _usersUnitOfWork.ChangePasswordAsync(user, model.CurrentPassword, model.NewPassword);
+        if (!result.Succeeded)
+        {
+            return BadRequest(result.Errors.FirstOrDefault()!.Description);
+        }
+
+        return NoContent();
+    }
 
     private TokenDTO BuildToken(User user)
     {
--- Orders/Orders.Frontend/Components/Pages/Auth/ChangePassword.razor.cs
+++ Orders/Orders.Frontend/Components/Pages/Auth/ChangePassword.razor.cs
@@ -1,6 +1,43 @@
+using Microsoft.AspNetCore.Components;
+
+using MudBlazor;
+
+using Orders.Frontend.Repositories;
+using Orders.Shared.DTOs;
+
 namespace Orders.Frontend.Components.Pages.Auth;
 
 public partial class ChangePassword
 {
+    private ChangePasswordDTO changePasswordDTO = new();
+    private bool loading;
 
+    [Inject] private NavigationManager NavigationManager { get; set; } = null!;
+    [Inject] private IDialogService DialogService { get; set; } = null!;
+    [Inject] private ISnackbar Snackbar { get; set; } = null!;
+    [Inject] private IRepository Repository { get; set; } = null!;
+    [CascadingParameter] private IMudDialogInstance MudDialog { get; set; } = null!;
+
+    private async Task ChangePasswordAsync()
+    {
+        loading = true;
+        var responseHttp = await Repository.PostAsync("/api/accounts/changePassword", changePasswordDTO);
+        loading = false;
+        if (responseHttp.Error)
+        {
+            var message = await responseHttp.GetErrorMessageAsync();
+            Snackbar.Add(message!, Severity.Error);
+            return;
+        }
+
+        MudDialog.Cancel();
+        NavigationManager.NavigateTo("/EditUser");
+        Snackbar.Add("Contraseña Modificada con éxito.", Severity.Success);
+    }
+
+    private void ReturnAction()
+    {
+        MudDialog.Cancel();
+        NavigationManager.NavigateTo("/EditUser");
+    }
 }
